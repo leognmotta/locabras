9c9adc4eee8acd7ad167c8058d421c24975db678
Leonardo Motta <leo@domboscoatacado.com.br>
2019-01-17 14:35:40 -0500

    Fixed responsive menu

---
+++ javascript/dom.js
@@ -0,0 +1,37 @@
+// Declare variables for responsive Menu
+var menuBtn = document.querySelector('.menu-btn');
+var menu = document.querySelector('.menu');
+var menuNav = document.querySelector('.menu-nav');
+var navItem = document.querySelectorAll('.nav-item a');
+
+// Initial state of showMenu
+var showMenu = false;
+
+// responsive menu event function
+menuBtn.addEventListener('click', function() {
+  if (!showMenu) {
+    menuBtn.classList.add('close');
+    menu.classList.remove('hide');
+    menu.classList.add('show');
+    // set Menu state
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menu.classList.add('hide');
+    // set Menu state
+    showMenu = false;
+  }
+});
+
+navItem.forEach(function(item) {
+  item.addEventListener('click', function() {
+    if (showMenu) {
+      menuBtn.classList.remove('close');
+      menu.classList.remove('show');
+      menu.classList.add('hide');
+      // set Menu state
+      showMenu = false;
+    }
+  });
+});
